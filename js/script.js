91558234762a33361a547cc98f6ff942af8a7e49
musonera <musosoaimee@gmail.com>
2019-01-18 15:33:37 +0200

    add codes to vision

---
+++ js/script.js
@@ -0,0 +1,57 @@
+public class Account
+{
+    private double balance; //STATE
+    private double interestRate; //STATE
+    private double rate;//STATE
+
+    public Account()
+    {
+        balance = 0; 
+        interestRate = 0;
+    }
+
+    public Account(double amount, double interestRate)
+    {
+        balance = amount;   
+        rate = interestRate;
+
+    } 
+
+    public void deposit(double amount)
+    {
+        balance=balance+amount;
+    }
+
+    public void withdraw(double amount)
+    {
+        balance = balance - amount;
+    }
+
+    public void setInterest(double rate)
+    {
+        balance = balance + balance * rate;
+        //this.setInterst = setInterest;  
+        //setInterest = InterestRate / 12;
+    }
+    public double computeInterest(int n)
+    {
+        balance=Math.pow(balance*(1+rate),n/12); 
+        return balance;
+    }
+
+    public double getsetInterest()
+    {
+        return rate;
+    }
+
+    public double getBalance()
+    {
+        return balance;
+    }
+
+    public void close()
+    {
+        balance =0;
+    }
+
+}
