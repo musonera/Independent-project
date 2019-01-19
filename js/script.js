e3b84d7dabca8c94a193633c8707bdd5fad0f574
musonera <musosoaimee@gmail.com>
2019-01-19 09:48:27 +0200

    changing the styles

--- js/script.js
+++ js/script.js
@@ -1,57 +1,33 @@
-public class Account
-{
-    private double balance; //STATE
-    private double interestRate; //STATE
-    private double rate;//STATE
-
-    public Account()
-    {
-        balance = 0; 
-        interestRate = 0;
-    }
-
-    public Account(double amount, double interestRate)
-    {
-        balance = amount;   
-        rate = interestRate;
-
-    } 
-
-    public void deposit(double amount)
-    {
-        balance=balance+amount;
-    }
-
-    public void withdraw(double amount)
-    {
-        balance = balance - amount;
-    }
-
-    public void setInterest(double rate)
-    {
-        balance = balance + balance * rate;
-        //this.setInterst = setInterest;  
-        //setInterest = InterestRate / 12;
-    }
-    public double computeInterest(int n)
-    {
-        balance=Math.pow(balance*(1+rate),n/12); 
-        return balance;
-    }
-
-    public double getsetInterest()
-    {
-        return rate;
-    }
-
-    public double getBalance()
-    {
-        return balance;
-    }
-
-    public void close()
-    {
-        balance =0;
-    }
-
-}
+var BankAccount = {
+  balance: 0,
+  withdraw: function(amount) {
+      this.balance = this.balance - amount;
+  },
+  deposit: function(amount) {
+      this.balance = this.balance + amount;
+  }
+ };
+ $(document).ready(function() {
+ var newAccount = Object.create(BankAccount);
+ $("form#create").submit(function(event) {
+  event.preventDefault();
+  var inputtedName = $("input#name").val();
+  var inputtedBalance = parseInt($("input#initial").val());
+  newAccount.name = inputtedName;
+  newAccount.deposit(inputtedBalance);
+  $(".balance").text(newAccount.balance);
+ });
+ $("form#manage").submit(function(event) {
+  event.preventDefault();
+  var newBalance = parseInt($("input#deposit").val());
+  newAccount.deposit(newBalance);
+  $(".balance").text(newAccount.balance);
+ // $("#manage").unbind('submit');
+ });
+ $("form#withdraw").submit(function(event) {
+  event.preventDefault();
+  var newBalance = parseInt($("input#withdraw").val());
+  newAccount.withdraw(newBalance);
+  $(".balance").text(newAccount.balance);
+ });
+ });
